f316ee1f4835495857f1cae3b45c3e536d9f8ab5
cxqpfan@163.com <cxqpfan@163.com>
2017-05-30 06:52:55 +0800

    commit test

---
+++ src/main/java/com/lazyman/pickupweb/Version.java
@@ -0,0 +1,10 @@
+package com.lazyman.pickupweb;
+
+/**
+ * Created by chenxiaoqi on 2017/5/30.
+ */
+public class Version {
+    public static void main(String[] args) {
+        System.out.printf("1.0");
+    }
+}
---
+++ src/test/java/com/lazyman/pickupweb/test/Test.java
@@ -0,0 +1,14 @@
+package com.lazyman.pickupweb.test;
+
+/**
+ * Created by chenxiaoqi on 2017/5/30.
+ */
+public class Test {
+
+
+    @org.junit.Test
+    public void test()
+    {
+
+    }
+}
