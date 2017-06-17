e2fea736ffeca8f866de043cb86849cb2c48adad
cxqpfan@163.com <cxqpfan@163.com>
2017-06-17 10:39:16 +0800

    greeting

--- src/main/java/com/lazyman/pickupweb/GreetingController.java
+++ src/main/java/com/lazyman/pickupweb/GreetingController.java
@@ -1,7 +1,21 @@
 package com.lazyman.pickupweb;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
  * Created by chenxiaoqi on 2017/6/16.
  */
+@Controller
 public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value = "name",required = false,defaultValue = "World") String name, Model model){
+        model.addAttribute("name",name);
+        return "greeting";
+    }
+
+
 }
